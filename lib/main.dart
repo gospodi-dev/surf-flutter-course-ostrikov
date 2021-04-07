24a7455868b230604686fc036bb6c5241186ffa7
gospodi <gospodijazzet@gmail.com>
2021-04-07 16:53:32 +0300

    ryntimeType

--- lib/main.dart
+++ lib/main.dart
@@ -9,39 +9,41 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Название приложения',
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+      ),
       home: MyFirstWidget(),
     );
   }
 }
-
+/*
 class MyFirstWidget extends StatelessWidget {
+  var count = 0;
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text('Hello'),
-      ),
-    );
-  }
-}
+    runtypeReturn() {
+      return context.runtimeType;
+    }
 
-/*
+    runtypeReturn(); // always shows 1, since widget re-builds
+    return Container();
+  }
+*/
+
 class MyFirstWidget extends StatefulWidget {
   @override
   _MyFirstWidgetState createState() => _MyFirstWidgetState();
 }
 
 class _MyFirstWidgetState extends State<MyFirstWidget> {
-  int counter = 0;
   @override
   Widget build(BuildContext context) {
-    counter++;
-    print(counter);
-    return Container(
-      child: Center(
-        child: Text('Hello'),
-      ),
-    );
+    runtypeReturn() {
+      return context.runtimeType;
+    }
+
+    runtypeReturn(); // always shows 1, since widget re-builds
+    return Container();
   }
 }
-*/
