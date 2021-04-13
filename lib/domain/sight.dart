5315418368c1d14045a8c8a9b96d9968713e07c0
gospodi <gospodijazzet@gmail.com>
2021-04-13 20:15:49 +0300

    Пункты 1, 2

---
+++ lib/domain/sight.dart
@@ -0,0 +1,10 @@
+class Sight {
+  String name; // название достопримечательности
+  double lat; //координаты места
+  double lon; //координаты места
+  String url; // путь до фотографии в интернете
+  String details; // описание достопримечательности
+  String type; // тип достопримечательности
+
+  Sight({this.name, this.type, this.lat, this.lon, this.details, this.url});
+}
