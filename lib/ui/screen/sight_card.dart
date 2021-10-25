39facc714f73e5b796e7aa75f698cf9095a409b5
Gospodi <gospodijazzet@yandex.ru>
2021-10-25 20:30:12 +0300

    add SizedBox, AspectRatio

--- lib/ui/screen/sight_card.dart
+++ lib/ui/screen/sight_card.dart
@@ -11,92 +11,100 @@ class SightCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       child: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Stack(
-              children: [
-                Container(
-                  width: double.infinity,
-                  height: 96,
-                  child: ClipRRect(
+        child: Expanded(
+          child: Align(
+            alignment: Alignment.bottomCenter,
+            child: AspectRatio(
+              aspectRatio: 1.8,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Stack(
+                    children: [
+                      Container(
+                        width: double.infinity,
+                        height: 96,
+                        child: ClipRRect(
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(16),
+                              topRight: Radius.circular(16),
+                            ),
+                            child: Image.network(
+                              sight.url,
+                              fit: BoxFit.cover,
+                            )),
+                        decoration: BoxDecoration(
+                          color: Colors.black54,
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(16),
+                            topRight: Radius.circular(16),
+                          ),
+                        ),
+                      ),
+                      Align(
+                        child: Container(
+                          margin: EdgeInsets.only(top: 19, right: 18),
+                          child: Icon(
+                            Icons.favorite_border_outlined,
+                            color: Colors.white,
+                          ),
+                        ),
+                        alignment: Alignment.topRight,
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(top: 16, left: 16),
+                        child: Text(
+                          sight.type.toLowerCase(),
+                          style: textBoldWhite.copyWith(fontSize: 14),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Container(
+                    height: 96,
+                    decoration: BoxDecoration(
                       borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(16),
-                        topRight: Radius.circular(16),
-                      ),
-                      child: Image.network(
-                        sight.url,
-                        fit: BoxFit.cover,
-                      )),
-                  decoration: BoxDecoration(
-                    color: Colors.black54,
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(16),
-                      topRight: Radius.circular(16),
+                          bottomLeft: Radius.circular(16),
+                          bottomRight: Radius.circular(16)),
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          margin: EdgeInsets.only(
+                            top: 16,
+                            right: 16,
+                            left: 16,
+                          ),
+                          child: Text(
+                            sight.name,
+                            style: textTitle,
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(
+                            bottom: 16,
+                            right: 16,
+                            left: 16,
+                            top: 2,
+                          ),
+                          child: Text(
+                            sight.details,
+                            style: textDescription,
+                            overflow: TextOverflow.ellipsis,
+                            maxLines: 1,
+                          ),
+                        ),
+                      ],
                     ),
                   ),
-                ),
-                Align(
-                  child: Container(
-                    margin: EdgeInsets.only(top: 19, right: 18),
-                    child: Icon(
-                      Icons.favorite_border_outlined,
-                      color: Colors.white,
-                    ),
-                  ),
-                  alignment: Alignment.topRight,
-                ),
-                Container(
-                  margin: EdgeInsets.only(top: 16, left: 16),
-                  child: Text(
-                    sight.type.toLowerCase(),
-                    style: textBoldWhite.copyWith(fontSize: 14),
-                  ),
-                ),
-              ],
-            ),
-            ConstrainedBox(
-              constraints: BoxConstraints(maxWidth: 200),
-              child: Container(
-                height: 96,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(16),
-                      bottomRight: Radius.circular(16)),
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Container(
-                      margin: EdgeInsets.only(
-                        top: 16,
-                        right: 16,
-                        left: 16,
-                      ),
-                      child: Text(
-                        sight.name,
-                        style: textTitle,
-                      ),
-                    ),
-                    Container(
-                      margin: EdgeInsets.only(
-                        bottom: 16,
-                        right: 16,
-                        left: 16,
-                        top: 2,
-                      ),
-                      child: Text(
-                        sight.details,
-                        style: textDescription,
-                        overflow: TextOverflow.ellipsis,
-                        maxLines: 1,
-                      ),
-                    ),
-                  ],
-                ),
+                ],
               ),
             ),
-          ],
+          ),
         ),
       ),
     );
